5e303d8547360ab237e5a8229c1379b61d49e9b3
Harro van der Klauw <hvdklauw@gmail.com>
2015-08-27 11:08:38 +0200

    Added a api call to generate a graph of the migrations.

--- alembic_offline/__init__.py
+++ alembic_offline/__init__.py
@@ -1,6 +1,12 @@
 """alembic-offline public interface."""
 from .decorators import phased
 from .operations import execute_script
-from .api import get_migration_data, get_migrations_data
+from .api import get_migration_data, get_migrations_data, generate_migration_graph
 
-__all__ = [phased.__name__, execute_script.__name__, get_migration_data.__name__, get_migrations_data.__name__]
+__all__ = [
+    phased.__name__,
+    execute_script.__name__,
+    get_migration_data.__name__,
+    get_migrations_data.__name__,
+    generate_migration_graph.__name__
+]
---
+++ tests/graph/migrations/versions/base.py
@@ -0,0 +1,33 @@
+"""Test migration."""
+import os.path
+from sqlalchemy import INTEGER, VARCHAR, NVARCHAR, TIMESTAMP, Column, func
+from alembic import op
+
+from alembic_offline import phased, execute_script
+
+from tests.migrations.scripts import script
+
+revision = '1'
+down_revision = None
+some_attribute = 'some-base-value'
+
+
+@phased
+def upgrade():
+    """Upgrade."""
+    op.create_table(
+        'account',
+        Column('id', INTEGER, primary_key=True),
+        Column('name', VARCHAR(50), nullable=False),
+        Column('description', NVARCHAR(200)),
+        Column('timestamp', TIMESTAMP, server_default=func.now())
+    )
+    yield
+    op.execute("update account set name='some'")
+    yield
+    execute_script(os.path.splitext(script.__file__)[0] + '.py')
+
+
+def downgrade():
+    """Downgrade."""
+    pass
